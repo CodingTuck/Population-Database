c707fd53b82d49eef98c5b1a91f90178507237a8
mitch <mitch@DESKTOP-5RPTGKT>
2020-02-29 17:21:18 -0500

    Add project files.

---
+++ Population Database/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace Population_Database
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cityBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.cityBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.populationDataSet = new Population_Database.PopulationDataSet();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.cityBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
+            this.cityDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.popAsc = new System.Windows.Forms.Button();
+            this.popDesc = new System.Windows.Forms.Button();
+            this.citySort = new System.Windows.Forms.Button();
+            this.cityTableAdapter = new Population_Database.PopulationDataSetTableAdapters.CityTableAdapter();
+            this.tableAdapterManager = new Population_Database.PopulationDataSetTableAdapters.TableAdapterManager();
+            this.calcPopulation = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.cityBindingNavigator)).BeginInit();
+            this.cityBindingNavigator.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.cityBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.populationDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cityDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cityBindingNavigator
+            // 
+            this.cityBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.cityBindingNavigator.BindingSource = this.cityBindingSource;
+            this.cityBindingNavigator.CountItem = this.bindingNavigatorCountItem;
+            this.cityBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.cityBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem,
+            this.cityBindingNavigatorSaveItem});
+            this.cityBindingNavigator.Location = new System.Drawing.Point(0, 0);
+            this.cityBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.cityBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.cityBindingNavigator.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.cityBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.cityBindingNavigator.Name = "cityBindingNavigator";
+            this.cityBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
+            this.cityBindingNavigator.Size = new System.Drawing.Size(338, 25);
+            this.cityBindingNavigator.TabIndex = 0;
+            this.cityBindingNavigator.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorAddNewItem.Text = "Add new";
+            // 
+            // cityBindingSource
+            // 
+            this.cityBindingSource.DataMember = "City";
+            this.cityBindingSource.DataSource = this.populationDataSet;
+            // 
+            // populationDataSet
+            // 
+            this.populationDataSet.DataSetName = "PopulationDataSet";
+            this.populationDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(35, 22);
+            this.bindingNavigatorCountItem.Text = "of {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Total number of items";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorDeleteItem.Text = "Delete";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveFirstItem.Text = "Move first";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMovePreviousItem.Text = "Move previous";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "Position";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 23);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "Current position";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator1";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveNextItem.Text = "Move next";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveLastItem.Text = "Move last";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator2";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // cityBindingNavigatorSaveItem
+            // 
+            this.cityBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.cityBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("cityBindingNavigatorSaveItem.Image")));
+            this.cityBindingNavigatorSaveItem.Name = "cityBindingNavigatorSaveItem";
+            this.cityBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 22);
+            this.cityBindingNavigatorSaveItem.Text = "Save Data";
+            this.cityBindingNavigatorSaveItem.Click += new System.EventHandler(this.cityBindingNavigatorSaveItem_Click);
+            // 
+            // cityDataGridView
+            // 
+            this.cityDataGridView.AutoGenerateColumns = false;
+            this.cityDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.cityDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2});
+            this.cityDataGridView.DataSource = this.cityBindingSource;
+            this.cityDataGridView.Location = new System.Drawing.Point(11, 26);
+            this.cityDataGridView.Name = "cityDataGridView";
+            this.cityDataGridView.Size = new System.Drawing.Size(301, 220);
+            this.cityDataGridView.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "City";
+            this.dataGridViewTextBoxColumn1.HeaderText = "City";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "Population";
+            this.dataGridViewTextBoxColumn2.HeaderText = "Population";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // popAsc
+            // 
+            this.popAsc.Location = new System.Drawing.Point(11, 252);
+            this.popAsc.Name = "popAsc";
+            this.popAsc.Size = new System.Drawing.Size(97, 60);
+            this.popAsc.TabIndex = 2;
+            this.popAsc.Text = "Sort By Population Asc";
+            this.popAsc.UseVisualStyleBackColor = true;
+            this.popAsc.Click += new System.EventHandler(this.popAsc_Click);
+            // 
+            // popDesc
+            // 
+            this.popDesc.Location = new System.Drawing.Point(114, 252);
+            this.popDesc.Name = "popDesc";
+            this.popDesc.Size = new System.Drawing.Size(97, 60);
+            this.popDesc.TabIndex = 3;
+            this.popDesc.Text = "Sort By Population Desc";
+            this.popDesc.UseVisualStyleBackColor = true;
+            this.popDesc.Click += new System.EventHandler(this.popDesc_Click);
+            // 
+            // citySort
+            // 
+            this.citySort.Location = new System.Drawing.Point(217, 252);
+            this.citySort.Name = "citySort";
+            this.citySort.Size = new System.Drawing.Size(97, 60);
+            this.citySort.TabIndex = 4;
+            this.citySort.Text = "Sort City By Name";
+            this.citySort.UseVisualStyleBackColor = true;
+            this.citySort.Click += new System.EventHandler(this.citySort_Click);
+            // 
+            // cityTableAdapter
+            // 
+            this.cityTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.CityTableAdapter = this.cityTableAdapter;
+            this.tableAdapterManager.UpdateOrder = Population_Database.PopulationDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // calcPopulation
+            // 
+            this.calcPopulation.Location = new System.Drawing.Point(114, 318);
+            this.calcPopulation.Name = "calcPopulation";
+            this.calcPopulation.Size = new System.Drawing.Size(97, 60);
+            this.calcPopulation.TabIndex = 7;
+            this.calcPopulation.Text = "Calculate Avg, Total, Highest and Lowest Population";
+            this.calcPopulation.UseVisualStyleBackColor = true;
+            this.calcPopulation.Click += new System.EventHandler(this.calcPopulation_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 389);
+            this.Controls.Add(this.calcPopulation);
+            this.Controls.Add(this.citySort);
+            this.Controls.Add(this.popDesc);
+            this.Controls.Add(this.popAsc);
+            this.Controls.Add(this.cityDataGridView);
+            this.Controls.Add(this.cityBindingNavigator);
+            this.Name = "Form1";
+            this.Text = "Population Database";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cityBindingNavigator)).EndInit();
+            this.cityBindingNavigator.ResumeLayout(false);
+            this.cityBindingNavigator.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.cityBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.populationDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cityDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PopulationDataSet populationDataSet;
+        private System.Windows.Forms.BindingSource cityBindingSource;
+        private PopulationDataSetTableAdapters.CityTableAdapter cityTableAdapter;
+        private PopulationDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.BindingNavigator cityBindingNavigator;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.ToolStripButton cityBindingNavigatorSaveItem;
+        private System.Windows.Forms.DataGridView cityDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.Button popAsc;
+        private System.Windows.Forms.Button popDesc;
+        private System.Windows.Forms.Button citySort;
+        private System.Windows.Forms.Button calcPopulation;
+    }
+}
+
---
+++ Population Database/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Population_Database
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cityBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.cityBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.populationDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'populationDataSet.City' table. You can move, or remove it, as needed.
+            this.cityTableAdapter.Fill(this.populationDataSet.City);
+
+        }
+
+        private void popAsc_Click(object sender, EventArgs e)
+        {
+            this.cityTableAdapter.FillByPopulationAsc(this.populationDataSet.City);
+        }
+
+        private void popDesc_Click(object sender, EventArgs e)
+        {
+            this.cityTableAdapter.FillByPopulationDesc(this.populationDataSet.City);
+        }
+
+        private void citySort_Click(object sender, EventArgs e)
+        {
+            this.cityTableAdapter.FillByCityName(this.populationDataSet.City);
+        }
+
+        private void calcPopulation_Click(object sender, EventArgs e)
+        {
+            //declare float to hold avg population
+            float avgPopulation;
+
+            //get total population
+            avgPopulation = (float)this.cityTableAdapter.AvgPopulation();
+
+            //declare float to hold total population
+            float totalPopulation;
+
+            //get total population
+            totalPopulation = (float)this.cityTableAdapter.TotalPopulation();
+
+            //declare float to hold highest population
+            float highestPopulation;
+
+            //get highest population
+            highestPopulation = (float)this.cityTableAdapter.HighestPopulation();
+
+            //declare float to hold highest population
+            float lowestPopulation;
+
+            //get highest population
+            lowestPopulation = (float)this.cityTableAdapter.LowestPopulation();
+
+            //display total population
+            MessageBox.Show("Average population is: " + avgPopulation.ToString("0,##0.#\n\n")
+               + "Total Population is: " + totalPopulation.ToString("0,##0.#\n\n") + 
+               "Highest Population is: " + highestPopulation.ToString("0,##0.#\n\n") +
+               "Lowest Population is: " + lowestPopulation.ToString("0,##0.#"));
+        }
+    }
+}
